90f11ede476255a0b9cb1412bb516872c126f2c2
Lum <supakorn.s@protonmail.com>
2024-02-06 23:13:16 +0700

    Group deque tests into sections

--- test/test_deque.cpp
+++ test/test_deque.cpp
@@ -2,15 +2,17 @@
 
 #include "Deque.h"
 
-TEST_CASE("An empty deque has zero items.") {
+TEST_CASE("The number of items in the deque must be consistent with the number of insertions and removals.") {
     Deque<int> deque;
-    REQUIRE(deque.n() == 0);
-}
 
-TEST_CASE("Three insertions result in three items.") {
-    Deque<int> deque;
-    deque.push_back(1);
-    deque.push_back(1);
-    deque.push_back(1);
-    REQUIRE(deque.n() == 3);
+    SECTION("An empty deque has zero items.") {
+        REQUIRE(deque.n() == 0);
+    }
+
+    SECTION("Three insertions result in three items.") {
+        deque.push_back(1);
+        deque.push_back(1);
+        deque.push_back(1);
+        REQUIRE(deque.n() == 3);
+    }
 }
